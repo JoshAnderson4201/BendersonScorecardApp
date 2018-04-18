4367e3b6f01dc563145217a9653d2c764f42939c
JoshAnderson4201 <joshanderson4201@gmail.com>
2018-04-18 17:34:37 -0400

    Starts the full scorecard.

--- BendersonScorecard/app/src/main/java/com/example/ja160637/bendersonscorecard/FullScorecard.java
+++ BendersonScorecard/app/src/main/java/com/example/ja160637/bendersonscorecard/FullScorecard.java
@@ -1,9 +1,15 @@
 package com.example.ja160637.bendersonscorecard;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.GridLayout;
 import android.graphics.Point;
@@ -17,39 +23,55 @@ public class FullScorecard extends AppCompatActivity {
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_full_scorecard);
         Scorecard();
     }
 
     public void Scorecard() {
-        // get the width of the screen
-        Point size = new Point();
-        getWindowManager().getDefaultDisplay().getSize(size);
-        int width = size.x;
-        int height = size.y;
+        HorizontalScrollView.LayoutParams scrollParams = new HorizontalScrollView.LayoutParams(HorizontalScrollView.LayoutParams.MATCH_PARENT,
+                HorizontalScrollView.LayoutParams.MATCH_PARENT);
+        scrollParams.setMargins(20, 20, 20, 20);
+        HorizontalScrollView scrollView = new HorizontalScrollView(this);
+        scrollView.setBackgroundColor(Color.GRAY);
+        scrollView.setFillViewport(true);
+        LinearLayout.LayoutParams linearParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        linearParams.setMargins(0,20,0,20);
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setBackgroundColor(Color.BLUE);
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        linearLayout.setWeightSum(18);
+        linearLayout.setLayoutParams(linearParams);
 
-        // create the layout manager as a grid layout
-        GridLayout gridLayout = new GridLayout(this);
-        gridLayout.setColumnCount(11);
-        gridLayout.setRowCount(14);
-
-        // create the TextViews and add them to the GridLayout
-        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-        params.setMargins(5, 5, 5, 5);
-        views = new TextView[14][11];
-        for (int row = 0; row < 14; row++) {
-            for (int col = 0; col < 11; col++) {
-                views[row][col] = new TextView(this);
-                views[row][col].setLayoutParams(params);
-                views[row][col].setTextSize((int)(width * .2));
-                views[row][col].setBackgroundColor(Color.parseColor("#ff0000"));
-                gridLayout.setPadding(50, 250, 50, 100);
-                gridLayout.addView(views[row][col], width,  height);
-            }
+        for(int i = 0; i < 18; i++)
+        {
+            TextView tv = new TextView(this);
+            tv.setBackgroundColor(Color.BLACK);
+            LinearLayout.LayoutParams tvParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            tvParams.setMargins(100,20,100,20);
+            tv.setLayoutParams(tvParams);
+            tv.setTextSize(20);
+            tv.setText(Integer.toString(i+1));
+            tv.setTextColor(Color.WHITE);
+            linearLayout.addView(tv);
         }
 
-        // set gridlayout as the view of this activity
-        setContentView(gridLayout);
+        for(int i = 0; i < 18; i++)
+        {
+            TextView tv = new TextView(this);
+            tv.setBackgroundColor(Color.BLACK);
+            LinearLayout.LayoutParams tvParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            tvParams.setMargins(100,40,100,20);
+            tv.setLayoutParams(tvParams);
+            tv.setTextSize(20);
+            tv.setText(Integer.toString(i+1));
+            tv.setTextColor(Color.WHITE);
+            linearLayout.addView(tv);
+        }
 
-
+        scrollView.addView(linearLayout);
+        setContentView(scrollView);
     }
 }
